639c294b7c05c2deb2396f08207047776c5a8c5c
NoamRa <noamraby@gmail.com>
2020-03-19 21:24:40 +0200

    use async in import

--- src/__test__/retinaStateInstance.test.ts
+++ src/__test__/retinaStateInstance.test.ts
@@ -1,14 +1,13 @@
 import retinaStateInstace from "../retinaStateInstance";
 
 describe("Text retinaStateInstace", () => {
-  it("should act as singleton", () => {
-    import("../retinaStateInstance").then(secondImport => {
-      const anotherRetinaStateInstance = secondImport.default;
-      expect(retinaStateInstace).toBe(anotherRetinaStateInstance);
+  it("should act as singleton", async () => {
+    const secondImport = await import("../retinaStateInstance");
+    const anotherRetinaStateInstance = secondImport.default;
+    expect(retinaStateInstace).toBe(anotherRetinaStateInstance);
 
-      const myObj = { a: 1 };
-      retinaStateInstace.setValue("myKey", myObj)
-      expect(anotherRetinaStateInstance.getValue("myKey")).toBe(myObj);
-    });
+    const myObj = { a: 1 };
+    retinaStateInstace.setValue("myKey", myObj);
+    expect(anotherRetinaStateInstance.getValue("myKey")).toBe(myObj);
   });
 });
